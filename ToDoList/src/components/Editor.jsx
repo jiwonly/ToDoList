37524fd20bf81c601fb15f3843e2aaffebd872b4
Jun Jiwon <ysjjw2003@naver.com>
2024-09-20 14:10:05 +0900

    ADD create todo

---
+++ ToDoList/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      // enter 키 누르면
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent(""); // 새로운 투두를 추가하고 나서 검색창 지우기
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
